42b015dafb12e9522632ed8e6546b03a2d0a915c
everyzzz <jquisper2@gmail.com>
2022-12-20 21:20:12 -0500

    add changes viewexpired

--- versionedPago/v2/api.py
+++ versionedPago/v2/api.py
@@ -6,14 +6,14 @@ from versionedPago.models import Services, ExpiredPayments, PaymentUser
 from versionedPago.v2.pagination import SimplePagination
 
 from rest_framework import filters
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 class ServicesView(ModelViewSet):
     queryset = Services.objects.all()
     serializer_class = ServicesSerializers
     http_method_names = ['get']
 
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     throttle_scope = 'services'
 
 class PaymentUsersView(ModelViewSet):
@@ -26,12 +26,63 @@ class PaymentUsersView(ModelViewSet):
     permission_classes = [AllowAny]
 
     throttle_scope = 'pagos'
-    
+
+from django.shortcuts import get_object_or_404
 class ExpiredPaymentView(ModelViewSet):
     queryset = ExpiredPayments.objects.all()
-    serializer_class = ExpiredPaymentsSerializers
+    #serializer_class = ExpiredPaymentsSerializers
     pagination_class = SimplePagination
-    http_method_names = ['get','post']
-    permission_classes = [AllowAny]
+    #http_method_names = ['get','post']
+    #permission_classes = [AllowAny]
     throttle_scope = 'expired'
-    
+
+    def get_serializer_class(self):
+        return ExpiredPaymentsSerializers
+
+    def list(self, request):
+        page = self.paginate_queryset(self.queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+            
+        serializer = self.get_serializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        if isinstance(request.data, list):
+            serializer = ExpiredPaymentsSerializers(data=request.data, many = True)
+        else:
+            serializer = ExpiredPaymentsSerializers(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        todo = get_object_or_404(self.queryset, pk=pk)
+        serializer = ExpiredPaymentsSerializers(todo)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        todo = get_object_or_404(self.queryset, pk=pk)
+        serializer = ExpiredPaymentsSerializers(todo, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        todo = get_object_or_404(self.queryset, pk=pk)
+        serializer = ExpiredPaymentsSerializers(todo, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        todo = get_object_or_404(self.queryset, pk=pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
