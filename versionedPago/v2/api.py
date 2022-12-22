8adb16882172ab51df72318ca8de3c09890ecfd3
everyzzz <jquisper2@gmail.com>
2022-12-22 09:56:07 -0500

    add permissions

--- versionedPago/v2/api.py
+++ versionedPago/v2/api.py
@@ -7,66 +7,60 @@ from versionedPago.v2.pagination import SimplePagination
 
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+#from django.shortcuts import get_object_or_404
 
-# TODO: User
+# TODO: Services User
 class ServicesViewUser(ModelViewSet):
-    """"Esta es una vista para el usuario"""
     queryset = Services.objects.all()
-    serializer_class = ServicesSerializers
-    http_method_names = ['get']
 
-    permission_classes = [IsAuthenticated]
     throttle_scope = 'services'
 
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy' or self.action == 'retrieve' or self.action == 'create':
+            permission_classes = [IsAdminUser]
+        return [permissions() for permissions in permission_classes]
+    def get_serializer_class(self):
+        return ServicesSerializers
+
+
+# TODO: Payment Users
 class PaymentUsersViewUser(ModelViewSet):
-    """"Esta es una vista para el usuario"""
     queryset = PaymentUser.objects.all()
-    serializer_class = PaymentUsersSerializers
     pagination_class = SimplePagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['payment_date', 'expiration_date']
-    http_method_names = ['get', 'post']
-    permission_classes = [IsAuthenticated]
-
+    
     throttle_scope = 'pagos'
 
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'list' or self.action == 'retrieve' or self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permissions() for permissions in permission_classes]
+
+    def get_serializer_class(self):
+        return PaymentUsersSerializers
+
+
+# TODO: Expired Payment
 class ExpiredPaymentViewUser(ModelViewSet):
-    """"Esta es una vista para el usuario"""
     queryset = ExpiredPayments.objects.all()
-    #serializer_class = ExpiredPaymentsSerializers
     pagination_class = SimplePagination
-    http_method_names = ['get','post']
-    permission_classes = [IsAuthenticated]
+
     throttle_scope = 'expired'
 
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'list' or self.action == 'retrieve' or self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        elif self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy' or self.action == 'retrieve':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
-# TODO: Admin
-class ServicesViewAdmin(ModelViewSet):
-    """"Esta es una vista para el admin"""
-    queryset = Services.objects.all()
-    serializer_class = ServicesSerializers
-    #http_method_names = ['get']
-
-    permission_classes = [IsAdminUser]
-    throttle_scope = 'services'
-
-class PaymentUsersViewAdmin(ModelViewSet):
-    """"Esta es una vista para el admin"""
-    queryset = PaymentUser.objects.all()
-    serializer_class = PaymentUsersSerializers
-    pagination_class = SimplePagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['payment_date', 'expiration_date']
-
-    permission_classes = [IsAdminUser]
-
-    throttle_scope = 'pagos'
-
-class ExpiredPaymentViewAdmin(ModelViewSet):
-    """"Esta es una vista para el admin"""
-    queryset = ExpiredPayments.objects.all()
-    #serializer_class = ExpiredPaymentsSerializers
-    pagination_class = SimplePagination
-    #http_method_names = ['get','post']
-    permission_classes = [IsAdminUser]
-    throttle_scope = 'expired'
+    def get_serializer_class(self):
+        return ExpiredPaymentsSerializers
--- versionedPago/v2/urls.py
+++ versionedPago/v2/urls.py
@@ -1,16 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from versionedPago.v2.api import ExpiredPaymentViewUser, PaymentUsersViewUser, ServicesViewUser
-from versionedPago.v2.api import ExpiredPaymentViewAdmin, PaymentUsersViewAdmin, ServicesViewAdmin
 
 route = DefaultRouter()
-# Todo: User
-route.register("services/user", ServicesViewUser, basename = "services-user")
-route.register("payment-users/user", PaymentUsersViewUser, basename = "payment-user-user")
-route.register("expired-payment/user", ExpiredPaymentViewUser, basename = "expired-payment-user")
 
-# Todo: Admin
-route.register("services/admin", ServicesViewAdmin, basename = "services-admin")
-route.register("payment-users/admin", PaymentUsersViewAdmin, basename = "payment-user-admin")
-route.register("expired-payment/admin", ExpiredPaymentViewAdmin, basename = "expired-payment-admin")
+route.register("services", ServicesViewUser, basename = "services")
+route.register("payment-users", PaymentUsersViewUser, basename = "payment-user")
+route.register("expired-payment", ExpiredPaymentViewUser, basename = "expired-payment")
 
 urlpatterns = route.urls
